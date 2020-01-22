5b93387562ef5e100975e6d59932fafee8ea820b
noeger <benjamin.claudio@gmail.com>
2020-01-22 11:05:04 +0100

    complete app version 6

--- app/src/main/java/com/example/securitree_app3/MainActivity.java
+++ app/src/main/java/com/example/securitree_app3/MainActivity.java
@@ -41,59 +41,8 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        //listView = (ListView) findViewById(R.id.listView_home);
-        //getJSON("http://10.0.2.2/php login/getdata.php");
+
     }
 
-    /*
-    private void getJSON(final String urlWebService) {
-
-        class GetJSON extends AsyncTask<Void, Void, String> {
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-            }
-
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            protected String doInBackground(Void... voids) {
-                try {
-                    URL url = new URL(urlWebService);
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    StringBuilder sb = new StringBuilder();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    String json;
-                    while ((json = bufferedReader.readLine()) != null) {
-                        sb.append(json + "\n");
-                    }
-                    return sb.toString().trim();
-                } catch (Exception e) {
-                    return null;
-                }
-            }
-        }
-        GetJSON getJSON = new GetJSON();
-        getJSON.execute();
-    }
-
-    private void loadIntoListView(String json) throws JSONException {
-        JSONArray jsonArray = new JSONArray(json);
-        String[] sensor = new String[jsonArray.length()];
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject obj = jsonArray.getJSONObject(i);
-            sensor[i] = obj.getString("id");
-        }
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sensor);
-        listView.setAdapter(arrayAdapter);
-    }
-
-     */
 
 }
--- app/src/main/java/com/example/securitree_app3/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/securitree_app3/ui/home/HomeFragment.java
@@ -1,23 +1,32 @@
 package com.example.securitree_app3.ui.home;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.hardware.Sensor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.securitree_app3.R;
 
+import java.util.ArrayList;
+
 
 public class HomeFragment extends Fragment {
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_sensors, container, false);
         return root;
+
     }
 
 }
